21021764bdd422074c1693fb06df12d03f8ecfd6
Jegadeeswaran <jegadeeswaran.p@techtreeit.com>
2025-07-24 06:55:19 +0530

    sixth commit

--- LoggerService.swift
+++ LoggerService.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public class LoggerService {
     public static func logString(for strings:String) {
-        print("[Logger service print]-> \(strings)")
+        print("[Logger service]-> \(strings)")
     }
 }
